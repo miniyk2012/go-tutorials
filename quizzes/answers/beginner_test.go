641698641f72e183172201e1e525ba929f61dbc9
CreatCodeBuild <wangxuanzhealbert@gmail.com>
2018-12-04 01:25:26 -0800

    add quize

---
+++ quizzes/answers/beginner_test.go
@@ -0,0 +1,21 @@
+package quizzes
+
+import "testing"
+
+// https://tour.go-zh.org/basics/4  中文
+// https://tour.golang.org/basics/4 English
+func Test4(t *testing.T) {
+
+	add := func(x int, y int) int {
+		return x + y
+	}
+
+	sub := func(x int, y int) int {
+		return x - y
+	}
+
+	result := sub(add(1, 2), 3)
+	if result != 0 {
+		t.FailNow()
+	}
+}
