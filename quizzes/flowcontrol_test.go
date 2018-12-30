65d7a4849153369f52c81c57884c18cbacfcdde8
mint64 <wangxuanzhealbert@gmail.com>
2018-12-30 09:34:43 -0800

    new quiz

---
+++ quizzes/flowcontrol_test.go
@@ -0,0 +1,14 @@
+package quizzes
+
+import "testing"
+
+// https://tour.golang.org/flowcontrol/1
+func Test1(t *testing.T) {
+	weightsOfSheep := []int{100, 200, 324, 12, 4, 543, 144}
+
+	// Count the number of sheep. During the counting, if the current sheep is odd, print out the weight of that sheep.
+	// 2nd, 4th, 6th, 8th, 10th ...
+	//for ; ; {
+	//
+	//}
+}
